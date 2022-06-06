2cf3f1abb3eb76f78d1b019f3d12a8fb7f7aed86
RylakRila <kylvark@gmail.com>
2022-06-06 22:08:44 +0800

    successfully connected to database

--- Week5/Server/Config/app.ts
+++ Week5/Server/Config/app.ts
@@ -4,9 +4,27 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
+// Step 1 - import db package
+import mongoose from 'mongoose';
+
+// import the router data
 import indexRouter from '../Routes/index';
 
 const app = express();
+
+// Step 2 - Complete the DB Configuration
+import * as DBConfig from './db';
+mongoose.connect(DBConfig.LocalURI);
+const db = mongoose.connection; // alias for mongoose connection
+
+// Step 3 - Listen for connections or Errors
+db.on("open", () => {
+  console.log(`Connected to MongoDB at ${DBConfig.HostName}`);
+});
+
+db.on("error", () => {
+  console.error(`Connection Error`);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, '../Views'));
