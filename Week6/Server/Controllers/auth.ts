fd9bd8cdcac9599192480d751584f72c1833728f
RylakRila <kylvark@gmail.com>
2022-06-13 21:16:14 +0800

    add authentication stubs

---
+++ Week6/Server/Controllers/auth.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+
+// Need passport functionality
+import passport from 'passport';
+
+// Need to include User Model for Authentication Function
+import User from '../Models/user';
+
+// Display Functions
+export function DisplayLoginPage(req: express.Request, res: express.Response, next: express.NextFunction) {
+    
+}
+
+export function DisplayRegisterPage(req: express.Request, res: express.Response, next: express.NextFunction) {
+    
+}
+
+// Processing Functions
+export function ProcessLoginPage(req: express.Request, res: express.Response, next: express.NextFunction) {
+    
+}
+
+export function ProcessRegisterPage(req: express.Request, res: express.Response, next: express.NextFunction) {
+    
+}
+
+export function ProcessLogoutPage(req: express.Request, res: express.Response, next: express.NextFunction) {
+    
+}
