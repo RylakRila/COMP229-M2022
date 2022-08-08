1e55b06b39162daa70485e6b1b33c18763417153
RylakRila <kylvark@gmail.com>
2022-08-08 19:07:47 -0400

    added v1 movielistdataservice

--- Front-End-APP/src/services/movieList-service.tsx
+++ Front-End-APP/src/services/movieList-service.tsx
@@ -1,1 +1,11 @@
 import http from '../components/http-common';
+import MovieModel from '../models/movie';
+import AuthHeader from './auth-header';
+
+class MovieListDataService {
+    readAll() {
+        return http.get<Array<MovieModel>>("/movie-list", AuthHeader());
+    }
+}
+
+export default new MovieListDataService();
