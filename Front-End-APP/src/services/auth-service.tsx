071bface5ed79a53e3c581dd1c0f955f9cd75a1a
RylakRila <kylvark@gmail.com>
2022-08-07 21:24:21 -0400

    added auth service

---
+++ Front-End-APP/src/services/auth-service.tsx
@@ -0,0 +1,32 @@
+import http from '../components/http-component';
+
+class AuthService {
+    public login(username: string, password: string) {
+        return http.post('/login', {username, password})
+        .then(response => {
+            if (response.data.token) {
+                localStorage.setItem("user", JSON.stringify(response.data));
+            }
+            
+            return response.data;
+        });
+    }
+    
+    public logout() {
+        localStorage.removeItem("user");
+    }
+    
+    public register(username: string, password: string, FirstName: string, LastName: string, EmailAddress: string) {
+        return http.post('/register', {username, password, FirstName, LastName, EmailAddress});
+    }
+    
+    public getCurrentUser() {
+        const userString = localStorage.getItem("user");
+        if (userString) {
+            return JSON.parse(userString);
+        }
+        return false;
+    }
+}
+
+export default new AuthService();
